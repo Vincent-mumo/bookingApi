22eddaac8cd02e1a2ce44066d660a5e5d5860810
vincent <vincent@kali.kali>
2023-08-05 12:30:13 +0300

    added cors link

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ mongoose.connection.on("disconnected",() => {
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: ["http://localhost:5173"],credentials: true}));
 app.use(cookieParser())
 
 //endpoints for accessing the API
